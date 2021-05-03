b4c83a666bf93e094c4eab87dffd0a9b0a1c4a35
jpclipffel <jpclipffel@users.noreply.github.com>
2021-05-03 20:32:01 +0200

    Rewrote `writefile` command
    * 'intelligent' open mode: when writing an event field which is of type
      `bytes`, file is automatically open in 'wb'
    * Cleanup open files both when terminatting pipeline (was already the
      case) and when hitting max open file limit
    * Syntax revamp
    * Doc

--- m42pl_commands/writefile.py
+++ m42pl_commands/writefile.py
@@ -1,63 +1,115 @@
+from textwrap import dedent
+from collections import OrderedDict
+
 from m42pl.commands import StreamingCommand
 from m42pl.event import Event
 from m42pl.fields import Field
 from m42pl.utils import formatters
 
 
-class WriteFile(StreamingCommand):
-    _about_     = 'Write events to a file'
-    _syntax_    = '[path=]{path} [[mode=](replace|append)]'
-    _aliases_   = ['writefile',]
+class _Write(StreamingCommand):
+    """Base class for `write` commands.
 
-    def __init__(self, path: str, mode: str = 'replace', *args):
+    Effective commands are defined at the end of this module.
+
+    This basic implementation does a simple caching of the open file
+    descriptors in :ivar:`cache`. As soon as it tries to open more file
+    than allowed by the OS, `target` catches an `OSError`, proceeds to
+    cleanup the open FD and retry.
+
+    The cache is also cleaned-up at the command's `__aexit__`.
+
+    Furthermore, the file open mode is defined as follow:
+
+    * If command is `write` or `writefile`, open mode starts with `w`
+      (write and truncate)
+    * If command is `write-append` or `writefile-append`, open mode
+      starts with 'a' (write and append)
+    * If the first event or event field written to a given path is
+      `bytes`, `b` (binary) is added to mode; otherwise, the mode is
+      not changed.
+
+    :ivar mode:         Write mode
+    :ivar formatter:    Event formatter; Default to `JsonFormatter`
+    :ivar cache:        File descriptors cache
+    """
+
+    _syntax_    = '({field name} to) {file path}'
+
+    _grammar_ = OrderedDict(StreamingCommand._grammar_)
+    _grammar_['start'] = dedent('''\
+        start: (field "to")? field
+    ''')
+
+    # Real mode is defined by children commands
+    mode = ''
+
+    class Transformer(StreamingCommand.Transformer):
+        def start(self, items):
+            return (), {
+                'path':  len(items) > 1 and items[1] or items[0],
+                'field': len(items) > 1 and items[0] or None
+            }
+
+    def __init__(self, path: str, field: str = None):
         """
         :param path:    File path
-        :param mode:    Write mode (`replace` or `append`)
-                        Defaults to `replace`.
+        :param field:   Field to write; If None, write complete event
         """
-        super().__init__(path, mode, *args)
+        super().__init__(path, field)
         self.path = Field(path)
-        self.mode = Field(mode, default='replace')
-        self.modes = {'replace': 'w', 'append': 'w+'}
+        self.field = field and Field(field, default='') or None
+        self.formatter = formatters.Json()
         self.cache = {}
-        self.formatter = formatters.Json(indent=2)
-
-    async def setup(self, event, pipeline):
-        # Setup file write mode
-        self.mode = self.modes.get(
-            (await self.mode.read(event, pipeline)).lower(),
-            'replace'
-        )
 
     async def format(self, event, pipeline):
+        if self.field:
+            return await self.field.read(event, pipeline)
         return self.formatter(event)
 
     async def target(self, event, pipeline):
+        # Get data and deduce open mode first
+        data = await self.format(event, pipeline)
+        mode = isinstance(data, bytes) and self.mode + 'b' or self.mode
+        # Get path
         path = await self.path.read(event, pipeline)
         # Open given file at `path` and add it to cache
         if path not in self.cache:
-            self.cache[path] = open(path, self.mode)
-        # Write event to file
-        self.cache[path].write(await self.format(event, pipeline))
+            try:
+                # pylint: disable=bad-open-mode
+                self.cache[path] = open(path, mode)
+            except OSError:
+                self.cleanup()
+                # pylint: disable=bad-open-mode
+                self.cache[path] = open(path, mode)
+        # Write to file
+        if isinstance(data, bytes):
+            self.cache[path].write(data)
+        else:
+            print(data, file=self.cache[path])
         # Done
         yield event
 
-    async def __aexit__(self, *args, **kwargs):
+    def cleanup(self):
+        """Closes the open file descriptors.
+        """
         for _, fd in self.cache.items():
             try:
                 fd.close()
             except Exception:
                 pass
 
+    async def __aexit__(self, *args, **kwargs):
+        self.cleanup()
 
-class WriteFileField(WriteFile):
-    _about_     = 'Write a single field to a file'
-    _syntax_    = f'{WriteFile._syntax_} [field=]<field>'
-    _aliases_   = ['writefile_field',]
 
-    def __init__(self, path: str, field: str, mode: str = 'replace'):
-        super().__init__(path, mode, field)
-        self.field = Field(field, default='')
+class Write(_Write):
+    _about_     = 'Write events or events field to a file (truncate)'
+    _aliases_   = ['write', 'writefile']
+    mode = 'w'
 
-    async def format(self, event, pipeline):
-        return await self.field.read(event, pipeline)
+
+class WriteAppend(_Write):
+    _about_     = 'Write events or events field to a file (append)'
+    _aliases_   = ['write-append', 'writefile-append']
+    mode = 'a'

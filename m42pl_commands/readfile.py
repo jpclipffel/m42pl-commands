435f103ae999f7ac4926c19c82430ddbe7b482af
jpclipffel <jpclipffel@users.noreply.github.com>
2021-05-03 20:27:32 +0200

    Updated `readfile` command
    * Files are now open in 'r' mode (was 'rb')
    * Remove the `mode={line|file}` option (reading lines implemented in
      command `readlines`)

--- m42pl_commands/readfile.py
+++ m42pl_commands/readfile.py
@@ -1,45 +1,38 @@
+from collections import OrderedDict
+from textwrap import dedent
+
 from m42pl.commands import GeneratingCommand
 from m42pl.event import Event
 from m42pl.fields import Field
 
 
 class ReadFile(GeneratingCommand):
-    _about_     = 'Read a local file'
+    _about_     = 'Read a text file'
     _aliases_   = ['readfile']
-    _syntax_    = '[path=]{file path} [field=]{dest field} [[mode=](file|line)]'
-    
-    def __init__(self, path: str, field: str = 'file', mode: str = 'line'):
+    _syntax_    = '{file path} (as {field name})'
+
+    _grammar_ = OrderedDict(GeneratingCommand._grammar_)
+    _grammar_['start'] = dedent('''\
+        // start: field ("as" field)? arguments
+        start: field ("as" field)?
+    ''')
+
+    class Transformer(GeneratingCommand.Transformer):
+        def start(self, items):
+            return items, {}
+
+    def __init__(self, path: str, field: str = 'file'):
         """
-        :param path:    File path
-        :param field:   Destination field
-        :param mode:    Read mode
-                        If mode is 'line', yields file line by line.
-                        If mode is 'file', yields file at once.
-                        Defaults to 'line'.
+        :param path:    Source file path
+        :param dest:    Destination field
         """
         super().__init__(path)
         self.path = Field(path)
         self.field = Field(field, default=field)
-        self.mode = Field(mode, default=mode)
     
     async def target(self, event, pipeline):
-        path = await self.path.read(event, pipeline)
-        mode = await self.mode.read(event, pipeline)
         try:
-            with open(path, 'r') as fd:
-                if mode == 'line':
-                    for chunk in fd.readlines():
-                        for line in chunk.splitlines():
-                            yield await self.field.write(event.derive(), line)
-                        # yield event.derive(data={
-                        #     'path': path,
-                        #     'line': line
-                        # })
-                elif mode == 'file':
-                    yield await self.field.write(event.derive(), fd.read())
-                    # yield event.derive(data={
-                    #     'path': path,
-                    #     'file': fd.read()
-                    # })
-        except Exception:
-            yield event.derive()
+            with open(await self.path.read(event, pipeline), 'r') as fd:
+                yield await self.field.write(event.derive(), fd.read())
+        except Exception as _error:
+            yield event

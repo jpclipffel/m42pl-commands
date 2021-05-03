a3ae6b3623949504bb159a81160ba231cab2571d
jpclipffel <jpclipffel@users.noreply.github.com>
2021-05-03 20:26:50 +0200

    Added results filtering option

--- m42pl_commands/cut.py
+++ m42pl_commands/cut.py
@@ -1,28 +1,44 @@
 import regex
 
 from m42pl.commands import StreamingCommand
-from m42pl.fields import Field
+from m42pl.fields import Field, FieldsMap
 
 
 class Cut(StreamingCommand):
     _about_     = 'Cut (split) a field using a regular expression'
-    _syntax_    = '{field} <regular expression>'
+    _syntax_    = '[field=]{field} [expr=]<regex> [[clean=]<yes|no>]'
     _aliases_   = ['cut', 'split']
 
-    def __init__(self, field, expr):
+    def __init__(self, field, expr: str, clean: bool = True):
         """
         :param field:   Source field to cut
         :param expr:    Regular expression
         """
-        super().__init__(field, expr)
+        super().__init__(field, expr, clean)
         self.field = Field(field)
-        self.expr = Field(expr)
+        self.fields = FieldsMap(**{
+            'expr': Field(expr),
+            'clean': Field(clean, default=True)
+        })
+        # self.expr = Field(expr)
+        # self.clean = Field(clean, default=False)
+
+    async def cut_filter(self, event, pipeline):
+        return list(filter(
+            None, 
+            self.expr.split(await self.field.read(event, pipeline))
+        ))
+    
+    async def cut_nofilter(self, event, pipeline):
+        return self.expr.split(await self.field.read(event, pipeline))
 
     async def setup(self, event, pipeline):
-        self.expr = regex.compile(await self.expr.read(event, pipeline))
+        fields = await self.fields.read(event, pipeline)
+        self.expr = regex.compile(fields.expr)
+        self.cutter = fields.clean and self.cut_filter or self.cut_nofilter
 
     async def target(self, event, pipeline):
         yield await self.field.write(
             event,
-            self.expr.split(await self.field.read(event, pipeline))
+            await self.cutter(event, pipeline)
         )

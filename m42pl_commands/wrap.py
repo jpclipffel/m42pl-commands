a0fa25b01d2f621d8d0738718a0e5e54749f0730
jpclipffel <jpclipffel@users.noreply.github.com>
2021-06-03 06:37:47 +0200

    [ADD] Added `wrap` command to "wrap" (enclose) fields into a (new) parent field

---
+++ m42pl_commands/wrap.py
@@ -0,0 +1,24 @@
+from m42pl.commands import StreamingCommand
+from m42pl.fields import Field
+from m42pl.event import Event
+
+
+class Wrap(StreamingCommand):
+    """Encapsulates (wraps) all fields into a new field.
+    """
+
+    _about_     = 'Wraps all fields into another field'
+    _syntax_    = '[field=]{field name}'
+    _aliases_   = ['wrap',]
+
+    def __init__(self, field = 'wrapped'):
+        """
+        :param field:   New field to wrap to.
+        """
+        self.field = field and Field(field, default=field) or None
+
+    async def target(self, event, pipeline):
+        if self.field:
+            yield await self.field.write(Event(), event.data)
+        else:
+            yield event

bb4645750c32f58f743d1674eb09c49ad61d4a6d
jpclipffel <jpclipffel@users.noreply.github.com>
2021-06-03 06:37:03 +0200

    [ADD] Added `split` command to "split" an event into sub-event by a field (unlike `expand`, each new event contains only the splited-by field value)

---
+++ m42pl_commands/split.py
@@ -0,0 +1,39 @@
+from copy import deepcopy
+
+from m42pl.commands import StreamingCommand
+from m42pl.fields import Field
+from m42pl.event import Event
+
+
+class Split(StreamingCommand):
+    """Splits a event's field's values into new events.
+    """
+
+    _about_     = 'Returns one new event per value for the given field'
+    _syntax_    = '[field=]{field name}'
+    _aliases_   = ['split', 'mvsplit']
+
+    def __init__(self, field: str, keys: list = []):
+        """
+        :param field:   Field to split on
+        :param keys:    New field names
+        """
+        super().__init__(field, keys)
+        self.field = Field(field, default=[], seqn=True)
+        self.keys = Field(keys, default=[], seqn=True)
+
+    async def target(self, event, pipeline):
+        keys = await self.keys.read(event, pipeline)
+        for i, item in enumerate(await self.field.read(event, pipeline)):
+            if isinstance(item, dict):
+                yield Event(data=item)
+            elif isinstance(item, (list, tuple)):
+                if len(keys):
+                    if i < len(keys):
+                        yield Event(data={
+                            keys[i]: item
+                        })
+                else:
+                    yield Event(data={
+                        await self.key.read(event, pipeline): item
+                    })

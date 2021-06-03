a24d08852bcd8970b84f6137bd0506ac3c5aa882
jpclipffel <jpclipffel@users.noreply.github.com>
2021-06-03 06:35:59 +0200

    [ADD] Added `fieldstats` command to report some info about event field (to be changed to a better format)

---
+++ m42pl_commands/fieldstats.py
@@ -0,0 +1,30 @@
+from m42pl.commands import BufferingCommand
+from m42pl.event import Event
+
+
+class FieldStats(BufferingCommand):
+    _aliases_   = ['fieldstats', 'fieldsstats', 'fstats']
+    _about_     = 'Compute fields statistics'
+
+    def __init__(self):
+        super().__init__()
+        self.stats = {}
+
+    async def setup(self, event, pipeline):
+        await super().setup(event, pipeline, maxsize=100000)
+
+    def stats_key(self, key, value):
+        if not isinstance(value, dict):
+            if not key in self.stats:
+                self.stats[key] = {
+                    'type': set()
+                }
+            self.stats[key]['type'].add(str(type(value).__name__))
+        else:
+            for k, v in value.items():
+                self.stats_key(f'{key}.{k}', v)
+
+    async def target(self, pipeline):
+        async for event in super().target(pipeline):
+            self.stats_key('', event.data)
+        yield Event(data=self.stats)

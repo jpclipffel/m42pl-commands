41148a25bc26f3aba40d915199a016b1292f6fbc
jpclipffel <jpclipffel@users.noreply.github.com>
2021-08-16 06:44:02 +0200

    Debug

--- m42pl_commands/tailf.py
+++ m42pl_commands/tailf.py
@@ -12,6 +12,7 @@ class Tailf(StreamingCommand):
         :param count:   Number or events to ignore.
                         Defaults to 1.
         """
+        super().__init__(count)
         self.count = Field(count, default=1)
     
     async def setup(self, event, pipeline):

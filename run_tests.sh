6b8713503e5c0f93ce5baa78b5317aebef20323b
jpclipffel <jpclipffel@users.noreply.github.com>
2021-11-28 19:35:37 +0100

    [Add] Added tests call script

---
+++ run_tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+PYTHON="python3"
+TESTD="$(dirname ${0})/tests"
+
+
+if [[ -d "${TESTD}" ]]; then
+    cd "${TESTD}"
+    "${PYTHON}" -m unittest test_*.py
+else
+    echo "Tests directory ${TESTD} not found"
+    exit 1
+fi

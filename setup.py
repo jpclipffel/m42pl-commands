682e2f0fb15e2b852078ec4d20386c50ed468708
jpclipffel <jpclipffel@users.noreply.github.com>
2021-12-31 16:39:45 +0100

    [Fix] Added version strings

--- setup.py
+++ setup.py
@@ -8,18 +8,19 @@ setup(
   version='1.0.0',
   packages=['m42pl_commands',],
   install_requires=[
-    'm42pl',
+    # 'm42pl',
     # ---
-    'msgpack',
-    'zmq',
-    'redis',
-    'requests',
-    'aiohttp',
-    'pygments',
-    'lxml',
-    'jsonpath-ng',
-    'jinja2',
-    'tabulate',
-    'types-tabulate'
+    # 'msgpack>=1.0.3',
+    # 'zmq',
+    'pyzmq>=22.3.0',
+    # 'redis>=4.0.2',
+    # 'requests',
+    'aiohttp>=3.8.1',
+    'pygments>=2.10.0',
+    'lxml>=4.7.1',
+    'jsonpath-ng>=1.5.3',
+    'jinja2>=3.0.3',
+    'tabulate>=0.8.9',
+    'types-tabulate>=0.8.3'
   ]
 )
